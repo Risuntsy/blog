f4166f91fe84e5afe6c574938aa30f4757b673eb
risun <fengwangtsy@gmail.com>
2024-01-09 20:25:38 +0800

    remove dead code

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
 import { markdownToTxt } from "markdown-to-txt";
 import { type CollectionEntry } from "astro:content";
-import { POST_PREVIEW_LENGTH } from "./config";
 
 export function isoDate(date: Date): string {
     return date.toISOString().substring(0, 10);
@@ -10,12 +9,6 @@ export function md2txt(md: string) {
     return markdownToTxt(md);
 }
 
-export function definePostPreview(post: CollectionEntry<"post">) {
-    Object.defineProperty(post.data, "preview", {
-        value: md2txt(post.body).substring(0, POST_PREVIEW_LENGTH),
-    });
-    return post;
-}
 
 export function postDateCompareReverse(
     a: CollectionEntry<"post">,
