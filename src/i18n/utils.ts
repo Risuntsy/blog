8a19816e7ac8309c9c00ac519c79b468bd4cfdbf
risun <fengwangtsy@gmail.com>
2024-01-15 11:36:43 +0800

    drop "get page from url"

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -5,26 +5,15 @@ export type Lang = keyof uiStruct;
 export type Page<T extends Lang> = keyof uiStruct[T];
 export type Key<T extends Lang, P extends Page<T>> = keyof uiStruct[T][P];
 
-export function getLangAndPageFromUrl(url: URL) {
-    const [_, lang, page] = url.pathname.split("/");
+export function getLangFromUrl(url: URL) {
+    const [_, lang] = url.pathname.split("/");
     if (lang in ui) {
-        const typedLang = lang as Lang;
-        if (page in ui[typedLang]) {
-            console;
-            const typedPage = page as Page<Lang>;
-            return {
-                lang: typedLang,
-                page: typedPage,
-            };
-        } else {
-            return {
-                lang: typedLang,
-            };
-        }
+        return {
+            lang: lang as Lang,
+        };
     }
     return {
         lang: defaultLang as Lang,
-        page: page as Page<Lang>,
     };
 }
 
